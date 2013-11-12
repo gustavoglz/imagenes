a99fb8878aed747acff22752f0280990f385032b
Gustavo González <gustavo.glz.r@gmail.com>
2013-11-12 09:42:55 -0600

    first commit

--- insertar.php
+++ insertar.php
@@ -7,28 +7,28 @@ $size = $_FILES["image"] ["size"];
 $temp = $_FILES["image"] ["tmp_name"];
 $error = $_FILES["image"] ["error"];
 
-$nombrecompuesto = date('His'). "-" .$name;
-
-if ($error > 0 )
-	die ("Error $error");
-else
-{
-	//$allowed = array ('image/pjpeg', 'image/jpeg', 'image/JPEG', 'image/JPG', 'image/X-PNG', 'image/PNG', 'image/png', 'image/x-png');
-    
-	$permitido = array ('image/pjpeg', 'image/jpeg', 'image/JPEG', 'image/JPG');
-	if (in_array($_FILES['image']['type'], $permitido)) 
-	{
-				move_uploaded_file($temp,"posters/".$nombrecompuesto);
-				
-	}
-		else
-	{
-		die ("El formato no es permitido o el archivo es muy grande");
-	}
-	
-	
-}
-$query = mysql_query("INSERT INTO  peliculas (titulo, fecha_estreno, sinopsis, poster) VALUES ('" . $_POST['titulo'] ."','" . $_POST['fecha_estreno'] . "','". $_POST['sinopsis'] . "','". $nombrecompuesto . "')");
-header('Location:index.php');
+//$nombrecompuesto = date('His'). "-" .$name;
+//
+//if ($error > 0 )
+//	die ("Error $error");
+//else
+//{
+//	//$allowed = array ('image/pjpeg', 'image/jpeg', 'image/JPEG', 'image/JPG', 'image/X-PNG', 'image/PNG', 'image/png', 'image/x-png');
+//    
+//	$permitido = array ('image/pjpeg', 'image/jpeg', 'image/JPEG', 'image/JPG');
+//	if (in_array($_FILES['image']['type'], $permitido)) 
+//	{
+//				move_uploaded_file($temp,"posters/".$nombrecompuesto);
+//				
+//	}
+//		else
+//	{
+//		die ("El formato no es permitido o el archivo es muy grande");
+//	}
+//	
+//	
+//}
+//$query = mysql_query("INSERT INTO  peliculas (titulo, fecha_estreno, sinopsis, poster) VALUES ('" . $_POST['titulo'] ."','" . $_POST['fecha_estreno'] . "','". $_POST['sinopsis'] . "','". $nombrecompuesto . "')");
+//header('Location:index.php');
 ?>
 
